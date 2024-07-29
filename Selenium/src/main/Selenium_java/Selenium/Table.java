05d37d4e4b4cef1c01df3fbde4bc86c843d0728b
Ajay-kumar5 <ajay915988@gmail.com>
2024-07-29 15:44:32 +0530

    commit

--- Selenium/src/main/Selenium_java/Selenium/Table.java
+++ Selenium/src/main/Selenium_java/Selenium/Table.java
@@ -18,29 +18,39 @@ public class Table {
 
         driver.get("https://demo.guru99.com/test/web-table-element.php");
 
-        List<WebElement> tab = driver.findElements(By.xpath("//table[@class = 'dataTable']/tbody/tr/td"));
+        driver.get("https://demo.guru99.com/test/web-table-element.php");
+
+        driver.manage().window().maximize();
+
+        List<WebElement> row=driver.findElements(By.xpath("//table[@class='dataTable']/tbody/tr"));
+
+        int rowsize=row.size();
+
+        List<WebElement> column=driver.findElements(By.xpath("//table[@class='dataTable']/tbody/tr[1]/td"));
+
+        int columnsize=column.size();
+
+        for(int i=1;i<=rowsize;i++)
 
 
-            for (int i = 0; i <tab.size(); i++)
+
+            for(int j=1;j<=columnsize;j++)
+
             {
-                System.out.println("company = " + tab.get(i).getText());
 
-                for (int j = 1; j < 2 ; j++) {
-                    System.out.println("Group  = " + tab.get(j).getText());
+                WebElement header=driver.findElement(By.xpath("//table[@class='dataTable']//th["+j+"]"));
 
-                    for (int z = 2; z < 3; z++) {
-                        System.out.println("Prev Price(Rs.)  = " + tab.get(z).getText());
+                String header1=header.getText();
 
-                        for (int a = 3; a < 4; a++) {
-                            System.out.println("current Price(Rs.)  = " + tab.get(a).getText());
+                WebElement data=driver.findElement(By.xpath("//table[@class='dataTable']/tbody/tr["+i+"]/td["+j+"]"));
 
-                            for (int b = 4; b < 5; b++) {
-                                System.out.println("% change  = " + tab.get(b).getText());
-                            }
-                        }
-                    }
-                }
+                String data1=data.getText();
+
+                System.out.println(header1+ ":"+data1);
+
             }
+
+        System.out.println("--------------------------------------------------------");
 
 
         driver.quit();
